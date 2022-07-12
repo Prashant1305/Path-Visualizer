477a8830592eec06f659a21a6b2dcaf8661486e4
Prashant <prashantsing13052000@gmail.com>
2022-07-12 12:21:58 +0530

    update 15

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Navbar from './.cph/component/Navbar';
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div className="App container">
+      <PathfindingVisualizer></PathfindingVisualizer>
+      {/* jsdafljladsjf */}
+      {/* <Navbar/> */}
+    </div>
+  );
+}
+
+export default App;
